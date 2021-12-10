70a55e31236eba37f7ffa93689257dbe7f508d3b
AhmedAQ11 <m.ahmedAQ.786@gmail.com>
2021-12-11 00:25:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,98 @@
+ //// CHAPTER OBJECT /////
+
+ /////  Q1 ///////
+
+
+
+// var itemsArray = [
+//     {
+//         name:"juice",
+//         price:"50",
+//         quantity:"3"
+//     },
+//     {
+//         name:"cookie",
+//         price:"30",
+//         quantity:"9"
+//     },
+//     {
+//         name:"shirt",
+//         price:"880",
+//         quantity:"1"
+//     },
+//     {
+//         name:"pen",
+//         price:"100",
+//         quantity:"2"
+//     }
+    
+// ];
+
+// var juice = itemsArray[0].price * itemsArray[0].quantity
+// var cookie = itemsArray[1].price * itemsArray[1].quantity
+// var shirt = itemsArray[2].price * itemsArray[2].quantity
+// var pen = itemsArray[3].price * itemsArray[3].quantity 
+// var totalPriceAllItems = juice + cookie + shirt + pen
+
+// console.log("total price of juice :",juice)
+// console.log("total price of cookie :",cookie)
+// console.log("total price of shirt :",shirt)
+// console.log("total price of pen :",pen)
+// console.log("total price of all items :" , totalPriceAllItems)
+
+
+
+
+
+//////////// Q2 //////////////
+
+
+// var object = {
+//     name : "Ahmed",
+//     email : "m.ahmed@gmail.com",
+//     password : 12345,
+//     age : 27,
+//     gender : "male",
+//     city : "karachi",
+//     country : "Pakistan"
+// }
+
+// var check = "age" in object
+// var check2 = "country" in object
+// var check3 = "firstName" in object
+// var check4 = "lastName" in object
+// console.log(check)
+// console.log(check2)
+// console.log(check3)
+// console.log(check4)
+
+
+
+
+/////// Q3 /////////////
+
+
+
+// function Std(stdName,stdAge,stdId,stdSection,stdClassName){
+//     this.stdName = stdName;
+//     this.stdAge = stdAge;
+//     this.stdId = stdId;
+//     this.stdSection = stdSection;
+//     this.stdClassName = stdClassName;
+// }
+
+// var std1 = new Std ("Ahmed", 20 , 110 , "morning" , "web mobile")
+// var std2 = new Std ("sir jaffer", 22 , 111 , "morning" , "javascript")
+// var std3 = new Std ("umer", 24 , 112 , "morning", "web developer")
+
+
+// console.log(std1)
+// console.log(std2)
+// console.log(std3)
+
+
+
+
+/////// Q4 //////
+
+
